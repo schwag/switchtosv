5836492f817d35a59afab57a791f96513b986567
schwag <justin@myriadcore.com>
2015-04-09 16:02:23 -0400

    Major Homepage Changes

--- index.php
+++ index.php
@@ -3,19 +3,45 @@
     <section class="headerBackground">
                 <div class="container">
                      <div class="row">
-                        <div class="introSection">
-                            <div class="col-md-4 topPad">
+                            <div class="col-md-5">
                                <div class="bubblePar">
-                                <h1 class="mainHeading">WELCOME TO ADVANCED FINISHING USA</h1>
-                                <p>Powder coating, Dye Sublimation coatings and wet painting are the preferred choice for quality-driven companies. The finishing experts at Advanced Finishing USA make it easy and affordable. We're the finishing touch you have been looking for.</p>
+                                <img class="homeLogo" src="img/logo.png" class="img-responsive">
+                                <h1 class="mainHeading">Welcome to Saint Vincent.</h1>
+                                 <h1 class="mainHeadingTwo">Welcome to better care.</h1>
+                                <p>Choosing the right primary care physician is one of the most important decisions you can make in life. That’s why we make it so simple to make the switch.</p>
+                                <div class="caption producthomeTitle">
+                                    <p><a href="#" class="btn btn-primary" role="button">Download Switch Kit</a></p>
+                                </div>
                                </div>
                             </div>
-                            <div class="col-md-8 topPad">
-                                <div class="bubbleImg">   
-                                  <img src="img/homepage_header_image.jpg" class="img-responsive">                                                     
-                                </div>
+                            <div class="col-md-7 homePicRight">
+                              <div class="meetPCP">
+                                <ul>
+                                  <li style="background-color: #00a651;">Meet a few of our PCP's</li>
+                                  <li><a href="">Dr. Thomas Slokan</a></h2>
+                                  <li><a href="">Dr. Chris Serafini</a></li>
+                                </ul>
+                              </div>
                             </div>
-                        </div>
+                    </div>
+                </div>
+    </section>
+
+    <section class="headerBackground">
+                <div class="container">
+                     <div class="row">
+                            <div class="col-md-7 homePicLeft">
+                            </div>
+                            <div class="col-md-5">
+                               <div class="bubblePar">
+                                <h1 class="mainHeading">Dr. Thomas Slokan</h1>
+                                 <h1 class="mainHeadingTwo">Welcome to better care.</h1>
+                                <p>Several years ago, Thomas Slokan, DO, was a structural engineer living in our nation’s capital. One day, he walked away and never looked back. According to Dr. Slokan, he left his job to attend medical school—and make a real difference in people’s lives. Dr. Slokan’s patients at Union City Family Practice couldn’t be happier with his decision.</p>
+                                <div class="caption producthomeTitle">
+                                    <p><a href="#" class="btn btn-primary" role="button">Download Switch Kit</a></p>
+                                </div>
+                               </div>
+                            </div>
                     </div>
                 </div>
     </section>
