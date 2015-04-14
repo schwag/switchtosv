1f6727c9d1eb77ddd9dedd296786f3a9c7711e44
schwag <justin@myriadcore.com>
2015-04-14 12:03:10 -0400

    Homepage Updates

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                                  <h1 class="mainHeadingTwo">Welcome to better care.</h1>
                                 <p>Choosing the right primary care physician is one of the most important decisions you can make in life. That’s why we make it so simple to make the switch.</p>
                                 <div class="caption producthomeTitle">
-                                    <p><a href="#" class="btn btn-primary" role="button">Download Switch Kit</a></p>
+                                    <p><a href="img/switchkit.pdf" target="_blank" class="btn btn-primary" role="button">Download Switch Kit</a></p>
                                 </div>
                                </div>
                             </div>
@@ -18,8 +18,8 @@
                               <div class="meetPCP">
                                 <ul>
                                   <li style="background-color: #00a651;">Meet a few of our PCP's</li>
-                                  <li><a href="">Dr. Thomas Slokan</a></h2>
-                                  <li><a href="">Dr. Chris Serafini</a></li>
+                                  <li><a class="docScroll" data-speed="380" data-target="#thomas" href="#thomas" style="color: #ffffff;">Dr. Thomas Slokan</a></h2></li>
+                                  <li><a class="docScroll" data-speed="380" data-target="#chris" href="#chris" style="color: #ffffff;">Dr. Chris Serafini</a></h2></li>
                                 </ul>
                               </div>
                             </div>
@@ -27,20 +27,40 @@
                 </div>
     </section>
 
-    <section class="headerBackground">
+    <section div id="thomas" class="headerBackground">  
                 <div class="container">
                      <div class="row">
-                            <div class="col-md-7 homePicLeft">
+                            <div class="col-md-7 homeDocLeft">
                             </div>
                             <div class="col-md-5">
                                <div class="bubblePar">
                                 <h1 class="mainHeading">Dr. Thomas Slokan</h1>
-                                 <h1 class="mainHeadingTwo">Welcome to better care.</h1>
                                 <p>Several years ago, Thomas Slokan, DO, was a structural engineer living in our nation’s capital. One day, he walked away and never looked back. According to Dr. Slokan, he left his job to attend medical school—and make a real difference in people’s lives. Dr. Slokan’s patients at Union City Family Practice couldn’t be happier with his decision.</p>
-                                <div class="caption producthomeTitle">
-                                    <p><a href="#" class="btn btn-primary" role="button">Download Switch Kit</a></p>
+                                <div>
+                                    <img src="img/tomvid.png" style="margin-bottom: 10px;" class= "img-responsive">
+
+                                    <p><a href="#" class="btn btn-primary" role="button">View Tom's story</a></p>
                                 </div>
                                </div>
+                            </div>
+                    </div>
+                </div>    
+    </section>
+
+    <section div id="chris" class="headerBackground">
+                <div class="container">
+                     <div class="row">
+                            <div class="col-md-5">
+                               <div class="bubblePar">
+                                <h1 class="mainHeading">Dr. Chris Serafini</h1>
+                                <p>Growing up in a household with five siblings, Chris Serafini, DO, always knew family would play an important role in his life. Born and raised in Erie, Dr. Serafini completed his residency at Saint Vincent. Soon after, he joined Drs. Joseph Deimel and Allison Snyder at Millcreek Family Practice, where he continues to put his patients and their families first.</p>
+                                <div>
+                                    <img src="img/chrisvid.jpg" style="margin-bottom: 10px;" class= "img-responsive">
+                                    <p><a href="#" class="btn btn-primary" role="button">View Chris's story</a></p>
+                                </div>
+                               </div>
+                            </div>
+                            <div class="col-md-7 homeDocRight">
                             </div>
                     </div>
                 </div>
